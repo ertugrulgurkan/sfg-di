00283b21e8a45fd8dd01b8cd8ff2871edf007ec8
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-21 18:58:15 +0300

    spring boot dependency injection

--- src/main/java/com/ertugrul/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/com/ertugrul/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,11 +1,13 @@
 package com.ertugrul.sfgdi.controllers;
 
 import com.ertugrul.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
 
     private final GreetingService greetingService;
-
 
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
