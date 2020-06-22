30c78c17109a2fe5338e2be5a326405af91f5b5a
ertugrulgurkan <ertugrulgurkan16@gmail.com>
2020-06-22 09:38:55 +0300

    Spring default profiles example

--- src/main/java/com/ertugrul/sfgdi/services/I18nTurkishGreetingService.java
+++ src/main/java/com/ertugrul/sfgdi/services/I18nTurkishGreetingService.java
@@ -4,7 +4,7 @@ package com.ertugrul.sfgdi.services;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile("TR")
+@Profile({"TR" , "default"})
 @Service("I18nService")
 public class I18nTurkishGreetingService implements GreetingService{
 
